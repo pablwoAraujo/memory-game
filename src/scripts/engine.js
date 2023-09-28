712ea5b4596a820dbc4f7b67cfe7031341b23156
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-09-28 20:08:36 -0300

    feat: criando os cards dinamicamente

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,6 +4,36 @@ const state = {
   actions: {},
 };
 
+const emojis = [
+  "🐶",
+  "🐶",
+  "🐱",
+  "🐱",
+  "🐭",
+  "🐭",
+  "🐼",
+  "🐼",
+  "🐮",
+  "🐮",
+  "🐷",
+  "🐷",
+  "🐸",
+  "🐸",
+  "🐵",
+  "🐵",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box);
+}
+
 function init() {}
 
 init();
